4405f7993034fcd0255b429475bfda7c58f260e8
Masaru Kitajima <thirstytraveler@mac.com>
2021-02-26 17:01:17 +0900

    Restart with Material Design Bootstrap as package.

--- api-bible-js/source/js/my_key.js
+++
@@ -1,5 +0,0 @@
-/*
- *   Copyright (c) 2021 Masaru Kitajima
- *   All rights reserved.
- */
-const API_KEY = `c27e1343097de8c7dd7498167c07c7a9`; // Fill this in with your own API key from https://scripture.api.bible/
